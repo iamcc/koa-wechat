4426a48449eedfe6e81e1c678c20b5b70d0e48b0
cc <sic.hcq@gmail.com>
2015-09-16 19:30:36 +0800

    empty

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 * @Author: CC
 * @Date:   2015-09-16 19:23:00
 * @Last Modified by:   CC
-* @Last Modified time: 2015-09-16 19:24:19
+* @Last Modified time: 2015-09-16 19:30:20
 */
 
 'use strict';
@@ -11,7 +11,7 @@ var app = require('koa')();
 var wechat = require('co-wechat');
 
 app.use(wechat('iamcc').middleware(function *() {
-  this.body = 'fuck';
+  this.body = this.req.body;
 }));
 
 app.listen(process.env.PORT || 3000);
