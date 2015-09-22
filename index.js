0aff438f02023f235e95b28b9d08524d5d5a9d1d
cc <sic.hcq@gmail.com>
2015-09-22 17:40:10 +0800

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 * @Author: CC
 * @Date:   2015-09-16 19:23:00
 * @Last Modified by:   CC
-* @Last Modified time: 2015-09-22 17:37:46
+* @Last Modified time: 2015-09-22 17:39:48
 */
 
 'use strict';
@@ -11,9 +11,10 @@ var app = require('koa')();
 var wechat = require('co-wechat');
 
 app.use(function *(next) {
-  yield wechat('iamcc').middleware(function *() {
+  var fn = wechat('iamcc').middleware(function *() {
     this.body = 'fuck'
-  })(next)
+  })
+  yield fn(next)
 });
 
 app.listen(process.env.PORT || 3000);
