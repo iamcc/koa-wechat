17ee41915690306060fbb49b9780b2cd67c1b482
cc <sic.hcq@gmail.com>
2015-09-22 17:47:30 +0800

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 * @Author: CC
 * @Date:   2015-09-16 19:23:00
 * @Last Modified by:   CC
-* @Last Modified time: 2015-09-22 17:42:21
+* @Last Modified time: 2015-09-22 17:47:25
 */
 
 'use strict';
@@ -10,8 +10,10 @@
 var app = require('koa')();
 var wechat = require('co-wechat');
 
-app.use(wechat('iamcc').middleware(function *() {
-  this.body = 'fuck'
-}));
+app.use(function *(next) {
+  return wechat('iamcc').middleware(function *() {
+    this.body = 'fuck'
+  })
+});
 
 app.listen(process.env.PORT || 3000);
